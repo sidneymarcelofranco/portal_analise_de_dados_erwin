14a1ef724441acf2d14a174bfd0cc33d963a9e2b
Sidney Marcelo Franco <sidneymarcelofranco@hotmail.com>
2025-07-28 21:38:32 -0300

    alter gitignore file add module

---
+++ app/key_vault.py
@@ -0,0 +1,9 @@
+SECRET_KEY = 'a3f4c8e2b1d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0'
+
+SQLSERVER_CONFIG = {
+    "DRIVER": "{ODBC Driver 17 for SQL Server}",
+    "SERVER": "192.168.0.1",
+    "DATABASE": "bancodedadosaqui",
+    "UID": "usuarioaqui",
+    "PWD": "senhaaqui"
+}
